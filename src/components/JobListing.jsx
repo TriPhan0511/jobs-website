180bc5e983900ccaf63f893b206f1658b200a124
Tri Phan <phanphutri@gmail.com>
2024-03-27 19:46:27 +0700

    Updated JobListing

--- src/components/JobListing.jsx
+++ src/components/JobListing.jsx
@@ -1,8 +1,16 @@
 import PropTypes from 'prop-types'
+import { useState } from 'react'
+import { FaMapMarker } from 'react-icons/fa'
 
 const JobListing = ({
   job: { id, type, title, description, salary, location },
 }) => {
+  const [showFullDescription, setShowFullDescription] = useState(false)
+
+  if (!showFullDescription) {
+    description = description.substring(0, 90) + '...'
+  }
+
   return (
     <div className='bg-white rounded-xl shadow-md relative'>
       <div className='p-4'>
@@ -11,11 +19,17 @@ const JobListing = ({
           <h3 className='text-xl font-bold'>{title}</h3>
         </div>
         <div className='mb-5'>{description}</div>
+        <button
+          className='text-indigo-500 mb-5 hover:text-indigo-600'
+          onClick={() => setShowFullDescription((prevState) => !prevState)}
+        >
+          {showFullDescription ? 'Less' : 'More'}
+        </button>
         <h3 className='text-indigo-500 mb-2'>{salary} / Year</h3>
         <div className='border border-gray-100 mb-5'></div>
         <div className='flex flex-col lg:flex-row justify-between mb-4'>
           <div className='text-orange-700 mb-3'>
-            <i className='fa-solid fa-location-dot text-lg'></i>
+            <FaMapMarker className='inline text-lg mb-1 mr-1' />
             {location}
           </div>
           <a
