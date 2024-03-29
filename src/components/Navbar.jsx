180bf0a4a6a3a27d499f2e60c256adb5da146b31
Tri Phan <phanphutri@gmail.com>
2024-03-29 15:25:31 +0700

    Updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,10 +2,11 @@ import { NavLink } from 'react-router-dom'
 import logo from '../assets/images/logo.png'
 
 const Navbar = () => {
-  let classes =
-    'text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2'
-  const myClasses = ({ isActive }) =>
-    isActive ? `bg-black ${classes}` : classes
+  const linkClass = ({ isActive }) => {
+    const styles =
+      'text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2'
+    return isActive ? `bg-black ${styles}` : styles
+  }
 
   return (
     <nav className='bg-indigo-700 border-b border-indigo-500'>
@@ -20,29 +21,13 @@ const Navbar = () => {
             </NavLink>
             <div className='md:ml-auto'>
               <div className='flex space-x-2'>
-                <NavLink
-                  to='/'
-                  className={myClasses}
-                  // className={({ isActive }) =>
-                  //   isActive
-                  //     ? 'bg-black text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2'
-                  //     : 'text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2'
-                  // }
-                >
+                <NavLink to='/' className={linkClass}>
                   Home
                 </NavLink>
-                <NavLink
-                  to='/jobs'
-                  className={myClasses}
-                  // className='text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2'
-                >
+                <NavLink to='/jobs' className={linkClass}>
                   Jobs
                 </NavLink>
-                <NavLink
-                  to='/add-job'
-                  className={myClasses}
-                  // className='text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2'
-                >
+                <NavLink to='/add-job' className={linkClass}>
                   Add Job
                 </NavLink>
               </div>
@@ -55,52 +40,3 @@ const Navbar = () => {
 }
 
 export default Navbar
-// import { NavLink } from 'react-router-dom'
-// import logo from '../assets/images/logo.png'
-
-// const Navbar = () => {
-//   return (
-//     <nav className='bg-indigo-700 border-b border-indigo-500'>
-//       <div className='mx-auto max-w-7xl px-2 sm:px-6 lg:px-8'>
-//         <div className='flex h-20 items-center justify-between'>
-//           <div className='flex flex-1 items-center justify-center md:items-stretch md:justify-start'>
-//             <NavLink className='flex flex-shrink-0 items-center mr-4' to='/'>
-//               <img className='h-10 w-auto' src={logo} alt='React Jobs' />
-//               <span className='hidden md:block text-white text-2xl font-bold ml-2'>
-//                 React Jobs
-//               </span>
-//             </NavLink>
-//             <div className='md:ml-auto'>
-//               <div className='flex space-x-2'>
-//                 <NavLink
-//                   to='/'
-//                   className={({ isActive }) =>
-//                     isActive
-//                       ? 'bg-black text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2'
-//                       : 'text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2'
-//                   }
-//                 >
-//                   Home
-//                 </NavLink>
-//                 <NavLink
-//                   to='/jobs'
-//                   className='text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2'
-//                 >
-//                   Jobs
-//                 </NavLink>
-//                 <NavLink
-//                   to='/add-job'
-//                   className='text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2'
-//                 >
-//                   Add Job
-//                 </NavLink>
-//               </div>
-//             </div>
-//           </div>
-//         </div>
-//       </div>
-//     </nav>
-//   )
-// }
-
-// export default Navbar
